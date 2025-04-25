6648acfa83e5bcac5fff7c0c3af388bd00494afe
Kana Sato <kanasato.it@gmail.com>
2025-04-25 22:48:55 +0900

    1.two-sum

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -4,6 +4,22 @@
  * [1] Two Sum
  */
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
 // @lc code=start
-function twoSum(nums: number[], target: number): number[] {}
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
 // @lc code=end
