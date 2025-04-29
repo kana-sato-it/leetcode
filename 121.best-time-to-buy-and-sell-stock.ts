c67c69c34ea53df06b29f5b8746dc1d451e28294
Kana Sato <kanasato.it@gmail.com>
2025-04-29 23:44:21 +0900

    121.best time to buy and sell stock

--- 121.best-time-to-buy-and-sell-stock.ts
+++ 121.best-time-to-buy-and-sell-stock.ts
@@ -6,7 +6,21 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
-    
-};
-// @lc code=end
+  let minPrice = Infinity;
+  let maxProfit = 0;
 
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    }
+
+    const profit = price - minPrice;
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+  }
+
+  return maxProfit;
+}
+
+// @lc code=end
