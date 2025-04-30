b5c3b2deefe8efa7641c6d357c8ccfcfa4c9e1de
Kana Sato <kanasato.it@gmail.com>
2025-04-30 23:28:17 +0900

    217.contains-dupulicate

---
+++ 217.contains-duplicate.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=217 lang=typescript
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+    
+};
+// @lc code=end
+
