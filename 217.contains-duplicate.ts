d430283314320db928a16b9d1df957be69dda192
Kana Sato <kanasato.it@gmail.com>
2025-05-01 23:50:42 +0900

    238

--- 217.contains-duplicate.ts
+++ 217.contains-duplicate.ts
@@ -6,7 +6,13 @@
 
 // @lc code=start
 function containsDuplicate(nums: number[]): boolean {
-    
-};
+  const numSet = new Set<number>();
+  for (const num of nums) {
+    if (numSet.has(num)) {
+      return true;
+    }
+    numSet.add(num);
+  }
+  return false;
+}
 // @lc code=end
-
