7077a3f1da49dcf3dbdbc4066d8cf3f7b8c28a57
Kana Sato <kanasato.it@gmail.com>
2025-05-03 22:29:45 +0900

    238

--- 238.product-of-array-except-self.ts
+++ 238.product-of-array-except-self.ts
@@ -6,7 +6,21 @@
 
 // @lc code=start
 function productExceptSelf(nums: number[]): number[] {
-    
-};
-// @lc code=end
+  const n = nums.length;
+  const result = new Array(n).fill(1);
 
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    result[i] *= leftProduct;
+    leftProduct *= nums[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return result;
+}
+// @lc code=end
